88df5d06592a660196fd14ef471ccd00d6ce4406
Theo0259 <122873258+Theo0259@users.noreply.github.com>
2023-06-27 18:42:00 +0200

    commit

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const replayBtn = document.getElementById("replayBtn");
 let number = Math.floor(Math.random() * 10) + 1;
 let attempts = 0;
 
+console.log(number);
 function numberTest() {
   const userInput = parseInt(document.getElementById("userInput").value);
   let output = "";
 
   if (userInput === number) {
     output = `<p><h4>Gagné ! 🙂</h4> <br> Le numéro mystère était : ${number}</p>`;
-    replayBtn.classList.remove("d-none");
-    btn.disabled = true;
+    setTimeout(function () {
+      replayBtn.classList.remove("d-none");
+      btn.disabled = true;
+    }, 1000);
   } else if (userInput > number) {
     output = `<p>${userInput} ? ... c'est ➖</p>`;
   } else {
@@ -22,20 +25,24 @@ function numberTest() {
 
   if (attempts === 1) {
     result.innerHTML += `<hr><p><h5>1er essai</h5></p>`;
+    setTimeout(function () {
+      result.innerHTML += output;
+    }, 1000);
   } else if (attempts === 2) {
     result.innerHTML += `<hr><p><h5>2ème essai</h5></p><br>`;
+    setTimeout(function () {
+      result.innerHTML += output;
+    }, 1000);
   } else if (attempts === 3 && userInput === number) {
     result.innerHTML += `<hr><p><h5>3ème essai</h5></p><br>`;
   } else {
     result.innerHTML += `<hr><p><h5>3ème essai</h5></p><br>`;
-    result.innerHTML += `<p><h4>Perdu...☹️</h4></p><br>${output}<br><p>Le numéro mystère était : ${number}</p>`;
-    btn.disabled = true;
-    replayBtn.classList.remove("d-none");
+    setTimeout(function () {
+      result.innerHTML += `<p><h4>Perdu...☹️</h4></p><br>${output}<br><p>Le numéro mystère était : ${number}</p>`;
+      btn.disabled = true;
+      replayBtn.classList.remove("d-none");
+    }, 1000);
   }
-
-  setTimeout(function () {
-    result.innerHTML += output;
-  }, 1000);
 }
 
 replayBtn.addEventListener("click", function () {
